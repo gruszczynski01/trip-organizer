ec3a55c2290baba0e34dc0ecd27fd4b36cb2c3ce
gruszczynski01 <gruszczynski01@gmail.com>
2020-10-13 17:28:35 +0200

    adding project main files and dirs

---
+++ trip-organizer-rn-app/screens/StartupScreen.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import {
+  View,
+  ActivityIndicator,
+  StyleSheet,
+  AsyncStorage,
+} from "react-native";
+import { useDispatch } from "react-redux";
+
+const StartupScreen = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default StartupScreen;
