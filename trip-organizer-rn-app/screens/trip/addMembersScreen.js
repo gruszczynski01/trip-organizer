3360a0b7e674efa74110c440f6eea06f1c6f947a
gruszczynski01 <gruszczynski01@gmail.com>
2020-10-21 00:15:28 +0200

    adding date picker pair to adding trip process

--- trip-organizer-rn-app/screens/trip/addMembersScreen.js
+++ trip-organizer-rn-app/screens/trip/addMembersScreen.js
@@ -1,17 +1,185 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { Text, View, StyleSheet, Button, FlatList } from "react-native";
+import Card from "../../components/technical/Card";
+import Input from "../../components/technical/Input";
 
-const addMembersScreen = () => {
+import DateTimePicker from "@react-native-community/datetimepicker";
+const cards = [
+  {
+    type: "card - 1",
+  },
+  {
+    type: "card - 2",
+  },
+  {
+    type: "card - 3",
+  },
+  {
+    type: "card - 4",
+  },
+  {
+    type: "card - 5",
+  },
+  {
+    type: "card - 6",
+  },
+  {
+    type: "card - 7",
+  },
+  {
+    type: "card - 8",
+  },
+  {
+    type: "card - 9",
+  },
+  {
+    type: "card - 10",
+  },
+  {
+    type: "card - 12",
+  },
+  {
+    type: "card - 13",
+  },
+  {
+    type: "card - 14",
+  },
+  {
+    type: "card - 15",
+  },
+  {
+    type: "card - 16",
+  },
+  {
+    type: "card - 17",
+  },
+  {
+    type: "card - 18",
+  },
+  {
+    type: "card - 19",
+  },
+];
+const addMembersScreen = (props) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || startDate;
+    setShow(Platform.OS === "ios");
+    setStartDate(currentDate);
+  };
+  const onChangeEnd = (event, selectedDate) => {
+    const currentDate = selectedDate || endDate;
+    setShow(Platform.OS === "ios");
+    setEndDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Hello, world! addMembersScreen</Text>
+    <View style={styles.screen}>
+      <Text style={styles.mainTitle}>Who are you going with?</Text>
+      <FlatList
+        bounces={false}
+        data={cards}
+        renderItem={({ index, item: { type } }) => (
+          <Card style={styles.cartItem}>
+            <Text style={styles.title}>Szymon Gruszczynski</Text>
+            <Text style={styles.emailText}>gruszczynski01@gmail.com</Text>
+          </Card>
+        )}
+        keyExtractor={(item) => item.index}
+      />
     </View>
   );
 };
+
+addMembersScreen.navigationOptions = {
+  headerTitle: "Select time period",
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "flex-start",
+    // alignItems: "center",
+  },
+  mainTitle: {
+    textTransform: "uppercase",
+    fontSize: 40,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    margin: 30,
+  },
+  title: {
+    textTransform: "uppercase",
+    fontSize: 25,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    margin: 30,
+  },
+  subtitle: {
+    textTransform: "uppercase",
+    fontSize: 15,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    marginLeft: 30,
+  },
+  input: {
+    width: "80%",
+    maxWidth: 400,
+    maxHeight: 400,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  buttonContainer: {
+    paddingVertical: 20,
+  },
+  container: { flex: 1 },
+  cartItem: {
+    padding: 10,
+    backgroundColor: "white",
+    // flexDirection: "row",
+    justifyContent: "flex-start",
+    marginHorizontal: 20,
+    marginTop: 30,
+    minHeight: 120,
+    flex: 1,
+    flexDirection: "column",
+  },
+  title: {
+    textTransform: "uppercase",
+    fontSize: 27,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    padding: 5,
+  },
+  emailText: {
+    fontSize: 17,
+    letterSpacing: 1,
+    padding: 5,
+  },
+  // destination: {
+  //   fontSize: 17,
+  //   letterSpacing: 1,
+  //   fontWeight: "bold",
+  //   paddingLeft: 5,
+  // },
+});
+
 export default addMembersScreen;
--- trip-organizer-rn-app/screens/trip/tripTimeScreen.js
+++ trip-organizer-rn-app/screens/trip/tripTimeScreen.js
@@ -1,61 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import Card from "../../components/technical/Card";
 import Input from "../../components/technical/Input";
-import { Calendar } from "react-native-calendars";
+// import { Calendar } from "react-native-calendars";
 import CalendarStrip from "react-native-calendar-strip";
+import Calendar from "react-native-calendar-select";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const tripTimeScreen = (props) => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || startDate;
+    setShow(Platform.OS === "ios");
+    setStartDate(currentDate);
+  };
+  const onChangeEnd = (event, selectedDate) => {
+    const currentDate = selectedDate || endDate;
+    setShow(Platform.OS === "ios");
+    setEndDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>When are you going?</Text>
       <Text style={styles.subtitle}>Trip begining</Text>
+
       <View>
-        <CalendarStrip
-          scrollable
-          style={{
-            height: 200,
-            paddingTop: 20,
-            paddingBottom: 10,
-            width: "100%",
-          }}
-          calendarColor={"#3343CE"}
-          calendarHeaderStyle={{ color: "white" }}
-          dateNumberStyle={{ color: "white" }}
-          dateNameStyle={{ color: "white" }}
-          iconContainer={{ flex: 0.1 }}
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={startDate}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+          minimumDate={new Date().getDate()}
         />
       </View>
       <Text style={styles.subtitle}>Trip ending</Text>
       <View>
-        <CalendarStrip
-          scrollable
-          style={{
-            height: 200,
-            paddingTop: 20,
-            paddingBottom: 10,
-            width: "100%",
-          }}
-          calendarColor={"#3343CE"}
-          calendarHeaderStyle={{ color: "white" }}
-          dateNumberStyle={{ color: "white" }}
-          dateNameStyle={{ color: "white" }}
-          iconContainer={{ flex: 0.1 }}
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={endDate}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChangeEnd}
+          minimumDate={startDate}
         />
       </View>
-      <Card style={styles.input}>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Next"
-            // color={Colors.accent}
-            onPress={() => {
-              props.navigation.navigate("TripTime");
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Next"
+          // color={Colors.accent}
+          onPress={() => {
+            props.navigation.navigate("AddMembers");
 
-              // setIsSignup((prevState) => !prevState);
-            }}
-          />
-        </View>
-      </Card>
+            // setIsSignup((prevState) => !prevState);
+          }}
+        />
+      </View>
     </View>
   );
 };
@@ -83,6 +104,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     letterSpacing: 1,
     margin: 30,
+  },
+  subtitle: {
+    textTransform: "uppercase",
+    fontSize: 15,
+    fontWeight: "bold",
+    letterSpacing: 1,
+    marginLeft: 30,
   },
   input: {
     width: "80%",
