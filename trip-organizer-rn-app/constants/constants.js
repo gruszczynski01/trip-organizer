7cd7091a032be3d373fa1859b6012df5ddbb005b
gruszczynski01 <gruszczynski01@gmail.com>
2020-10-30 01:08:42 +0100

    using firebase lib on all auth actions

--- trip-organizer-rn-app/constants/constants.js
+++ trip-organizer-rn-app/constants/constants.js
@@ -1,4 +1,4 @@
-export const FIREBASE_API_KEY = "ss";
+export const FIREBASE_API_KEY = "AIzaSyBr4f8mBJ1Era_ZF1hfWT1tQ41BZrkA_sE";
 export const FIREBASE_AUTH_DOMAIN = "trip-organizer-2020.firebaseapp.com";
 export const FIREBASE_DATABASE_URL =
   "https://trip-organizer-2020.firebaseio.com";
--- trip-organizer-rn-app/store/actions/auth.js
+++ trip-organizer-rn-app/store/actions/auth.js
@@ -4,7 +4,7 @@ import { AsyncStorage } from "react-native";
 // export const LOGIN = 'LOGIN';
 export const AUTHENTICATE = "AUTHENTICATE";
 export const LOGOUT = "LOGOUT";
-import { auth } from "../../firebase";
+import { auth, database } from "../../firebase";
 
 let timer;
 
@@ -45,45 +45,38 @@ const saveDataToStorage = (token, userId, expirationDate) => {
 };
 export const signup = (email, password, name, surname) => {
   return async (dispatch) => {
-    const response = await fetch(
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBr4f8mBJ1Era_ZF1hfWT1tQ41BZrkA_sE",
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email: email,
-          password: password,
-          returnScureToken: true,
-        }),
-      }
-    );
+    const response = await auth
+      .createUserWithEmailAndPassword(email, password)
+      .catch(function (error) {
+        const errorId = errorResData.error.message;
+        if (errorCode == "auth/weak-password") {
+          message = "The password is too weak.";
+        } else if (errorCode == "auth/operation-not-allowed") {
+          message = "Operation not allowed.";
+        } else if (errorCode == "auth/invalid-email") {
+          message = "Email address is not valid.";
+        } else if (errorCode == "auth/email-already-in-use") {
+          message = "Address email already in use";
+        } else {
+          message = errorMessage;
+        }
+        throw new Error(message);
+      });
 
-    if (!response.ok) {
-      const errorResData = await response.json;
-      const errorId = errorResData.error.message;
-      let message = "Something wend wrong!";
-      if (errorId === "EMAIL_EXIST") {
-        message = "This email exists already!";
-      }
-      throw new Error(message);
-    }
-
-    const resData = await response.json();
-    console.log(resData);
+    const resData = await JSON.parse(JSON.stringify(response));
     dispatch(
       authenticate(
-        resData.localId,
-        resData.idToken,
-        parseInt(resData.expiresIn) * 1000
+        resData.user.uid,
+        resData.user.stsTokenManager.accessToken,
+        parseInt(resData.user.stsTokenManager.expirationTime) * 1000
       )
     );
     const expirationDate = new Date(
-      new Date().getTime() + parseInt(resData.expiresIn) * 1000
+      new Date().getTime() +
+        parseInt(resData.user.stsTokenManager.expirationTime) * 1000
     );
-    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
-    dispatch(addUserToDatabase(resData.localId, email, name, surname));
+    saveDataToStorage(resData.user.uid, resData.user.uid, expirationDate);
+    dispatch(addUserToDatabase(resData.user.uid, email, name, surname));
   };
 };
 
@@ -127,40 +120,17 @@ export const login = (email, password) => {
 
 const addUserToDatabase = (id, email, name, surname) => {
   return async (dispatch, getState) => {
-    console.log(
-      `ID: ${id}, email: ${email}, name: ${name}, surname: ${surname}`
-    );
     const token = getState().auth.token;
     const userId = getState().auth.userId;
-    const response = await fetch(
-      `https://trip-organizer-2020.firebaseio.com/users/${id}.json?auth=${token}`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          email,
-          name,
-          surname,
-        }),
-      }
-    );
-
-    // if (!response.ok) {
-    //   const errorResData = await response.json;
-    //   console.log(errorResData);
-    // }
-    const resData = await response.json();
-    console.log("resData: " + resData);
-    // dispatch({
-    //   type: ADD_USER,
-    //   userData: {
-    //     id,
-    //     email,
-    //     name,
-    //     surname,
-    //   },
-    // });
+    const body = JSON.stringify({
+      email,
+      name,
+      surname,
+    });
+    const response = await database.ref("users/" + userId).set({
+      email: email,
+      name: name,
+      surname: surname,
+    });
   };
 };
